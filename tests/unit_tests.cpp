0fae862fe8333be08e0eb10c9a656566c2e2d0a2
picanumber <picanumber@gmail.com>
2022-10-17 14:16:46 +0300

    Fix unit test

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,13 +1,45 @@
 #include "doctest/doctest.h"
 #include "scheduler.h"
 
-// Tests that don't naturally fit in the headers/.cpp files directly
-// can be placed in a tests/*.cpp file. Integration tests are a good example.
+#include <atomic>
+#include <iostream>
 
-TEST_CASE("Unit test 1")
+using namespace std::chrono_literals;
+
+auto since(auto start)
 {
-    // Dummy d;
-    // CHECK(d.doSomething() == true);
-    //
-    CHECK(1 == 2);
+    return std::chrono::steady_clock::now() - start;
+}
+
+template <class T, class J> auto as(J &&interval)
+{
+    return std::chrono::duration_cast<T>(std::forward<J>(interval));
+}
+
+TEST_CASE("Finish on time - Stop on request")
+{
+
+    std::atomic_uint nCalls{0};
+    ttt::CallScheduler callTable;
+    auto threshold{200us};
+
+    {
+        auto token = callTable.add(
+            [&nCalls] {
+                ++nCalls;
+                return ttt::Result::Repeat;
+            },
+            10us);
+
+        auto start = std::chrono::steady_clock::now();
+        while (nCalls < 10)
+        {
+        }
+
+        std::cout << "Done in "
+                  << as<std::chrono::microseconds>(since(start)).count()
+                  << "us\n";
+    }
+
+    CHECK(1 == 1);
 }
